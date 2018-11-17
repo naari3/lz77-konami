510f1812389d82d2a3170e37084f1001c3c4a75f
naari3 <naari.named@gmail.com>
2018-11-17 21:03:42 +0900

    bundle gem lz77-konami

---
+++ lib/lz77/konami.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'lz77/konami/version'
+
+module LZ77
+  module Konami
+  end
+end
---
+++ lib/lz77/konami/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module LZ77
+  module Konami
+    VERSION = '0.1.0'
+  end
+end
